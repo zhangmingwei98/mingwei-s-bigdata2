9db3aa9920d2f3ecdffad15a8a918921a06173f2
zhangmingwei98 <zhangmingwei98@163.com>
2019-06-19 19:41:39 -0700

    uodate

---
+++ ssh-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#install ssh
+
+apt-get install -y ssh
+
+#configue sshd
+CONFIG=/etc/ssh/sshd_config
+sed -i s?"prohibit-password"?"yes"?g ${CONFIG}
+
+
+
+# restart ssh service
+systemctl restart sshd
+
+
